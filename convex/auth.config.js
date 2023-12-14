39b1aa2aa5868774e842359342a32dd5ca67eea9
girish chalumuri <girishchalumuri@gmail.com>
2023-12-14 07:45:58 +0530

    init: authorizer dev

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,8 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   providers: [
     {
-      domain: 'https://neat-crayfish-55.clerk.accounts.dev',
-      applicationID: 'convex',
+      domain: "https://authorizer-production-0c5f.up.railway.app",
+      applicationID: "845feae8-837d-4978-b45d-d7f7f62cb3bb",
     },
   ],
 };
